9d21c52f8378c8cf6ec6cb4496a1caa9295efd67
Drishti Jain <drishtijjain@gmail.com>
2023-02-21 17:00:08 +0530

    Add kinesis py file

---
+++ kinesis.py
@@ -0,0 +1,14 @@
+import boto3
+
+kinesis = boto3.client('kinesis')
+
+response = kinesis.create_stream(
+    StreamName='new-stream',
+    ShardCount=1
+)
+
+putData = kinesis.put_record(
+    StreamName='new-stream',
+    Data='Hello world',
+    PartitionKey='partitionKey'
+)
